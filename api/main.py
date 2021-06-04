d8c5a7cf363d2651f79afcc3640ac55f8f52de4c
abhi <abhi@kali.care>
2021-06-04 09:18:53 -0700

    Added API placeholder cloud function

---
+++ api/main.py
@@ -0,0 +1,24 @@
+import flask
+import json
+
+from google.cloud import bigquery
+
+PROJECT_ID = 'careintent'  # os.environ.get('GCP_PROJECT')  # Only for py3.7
+
+ALLOW_HEADERS = ['Accept', 'Authorization', 'Cache-Control', 'Content-Type', 'Cookie', 'Expires', 'Origin', 'Pragma',
+                 'Access-Control-Allow-Headers', 'Access-Control-Request-Method', 'Access-Control-Request-Headers',
+                 'Access-Control-Allow-Credentials', 'X-Requested-With']
+
+
+def api(request):
+    response = flask.make_response()
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    origin = request.headers.get('origin')
+    response.headers['Access-Control-Allow-Origin'] = origin if origin else '*'
+
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PATCH,DELETE'
+        response.headers['Access-Control-Allow-Headers'] = ', '.join(ALLOW_HEADERS)
+        response.status_code = 204
+
+    return response
