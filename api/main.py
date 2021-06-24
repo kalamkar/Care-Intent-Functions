7315e80063e28de0340ca57f0510c669db9b658f
abhi <abhi@kali.care>
2021-06-24 14:51:04 -0700

    Added relation and query apis

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import base64
 import datetime
 import dateutil.parser
 import flask
@@ -11,7 +12,8 @@ JSON_CACHE_SECONDS = 600
 ALLOW_HEADERS = ['Accept', 'Authorization', 'Cache-Control', 'Content-Type', 'Cookie', 'Expires', 'Origin', 'Pragma',
                  'Access-Control-Allow-Headers', 'Access-Control-Request-Method', 'Access-Control-Request-Headers',
                  'Access-Control-Allow-Credentials', 'X-Requested-With']
-RESOURCES = ['persons', 'relations', 'orgs', 'actions', 'content']
+RESOURCES = ['persons', 'groups', 'actions', 'contents']
+RELATION_TYPES = ['member_of', 'admin_of']
 
 
 def api(request):
@@ -35,7 +37,11 @@ def api(request):
     except:
         user = None
 
-    if len(tokens) >= 2 and tokens[1] in RESOURCES and user:
+    if len(tokens) >= 2 and tokens[1] == 'query' and user:
+        response = query(request, response, user)
+    elif len(tokens) >= 2 and tokens[1] == 'relate' and user and request.json:
+        response = add_relation(request, response, user)
+    elif len(tokens) >= 2 and tokens[1] in RESOURCES and user:
         response = resources(request, response, user)
     elif len(tokens) >= 3 and tokens[1] == 'data':
         start_time, end_time = get_start_end_times(request)
@@ -52,6 +58,43 @@ def api(request):
     response.headers['Content-Type'] = 'application/json'
     response.headers['Cache-Control'] = 'max-age=%d' % JSON_CACHE_SECONDS
     return response
+
+
+def query(request, response, user):
+    resource = request.args.get('resource').split(':', 1)
+    resource = {'type': resource[0], 'value': resource[1]}
+    relation_type = request.args.get('relation_type')
+    resource_type = request.args.get('resource_type')
+    result_type = 'target' if resource_type == 'source' else 'source'
+
+    results = []
+    db = firestore.Client()
+    for relation in db.collection('relations')\
+            .where(resource_type, '==', resource).where('type', '==', relation_type).get():
+        result_id = relation.get(result_type)
+        doc = db.collection(result_id['type'] + 's').document(result_id['value']).get().to_dict()
+        if 'login' in doc:
+            del doc['login']
+        results.append(doc)
+
+    return flask.jsonify({'results': results})
+
+
+def add_relation(request, response, user):
+    relation = request.json
+    if 'source' not in relation or 'target' not in relation or 'type' not in relation\
+            or relation['type'] not in RELATION_TYPES:
+        response.status_code = 400
+        return response
+
+    db = firestore.Client()
+    if relation['source']['type'] == 'person' and ':' in relation['source']['value']:
+        relation['source']['value'] = get_person_id(db, **relation['source']['value'].split(':', 1))
+    if relation['target']['type'] == 'person' and ':' in relation['target']['value']:
+        relation['target']['value'] = get_person_id(db, **relation['target']['value'].split(':', 1))
+    relation_id = base64.urlsafe_b64encode(uuid.uuid4().bytes).rstrip(b'=').decode('ascii')
+    db.collection('relations').document(relation_id).set(relation)
+    return flask.jsonify({'status': 'ok'})
 
 
 def resources(request, response, user):
@@ -120,3 +163,17 @@ def get_start_end_times(request):
     end_time = request.args.get('end')
     end_time = dateutil.parser.parse(end_time) if end_time else datetime.datetime.utcnow()
     return start_time.isoformat(), end_time.isoformat()
+
+
+def get_person_id(db, id_type, value):
+    identifier = {'type': id_type, 'value': value, 'active': True}
+    person_ref = db.collection('persons').where('identifiers', 'array_contains', identifier)
+    persons = list(person_ref.get())
+    if len(persons) == 0:
+        # Create new person since it doesn't exist
+        person_id = base64.urlsafe_b64encode(uuid.uuid4().bytes).rstrip(b'=').decode('ascii')
+        person = {'identifiers': [identifier]}
+        db.collection('persons').document(person_id).set(person)
+        return person_id
+
+    return persons[0].id
