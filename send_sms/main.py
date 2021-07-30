b4f71edfc752adc970fee88bae42c073fb60b7b5
abhi <abhi@kali.care>
2021-07-30 12:34:39 -0700

    Moved print message before twilio call to see it in case of failures

--- send_sms/main.py
+++ send_sms/main.py
@@ -13,9 +13,9 @@ def send_sms(request):
     message = json.loads(base64.b64decode(request.json['message']['data']).decode('utf-8'))
     if 'sender' not in message or not message['sender'] or 'value' not in message:
         message['sender'] = {'value': PHONE_NUMBER}
+    print(message)
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     client.messages.create(to=message['receiver']['value'], from_=message['sender']['value'],
                            body=message['content'])
-    print(message)
     return 'OK'
 
