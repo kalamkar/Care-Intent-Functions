479a4dd31c8b7394c33ce7590a6d39863553fbe0
abhi <abhi@kali.care>
2021-08-03 15:30:51 -0700

    Added error check for receiver in send sms

--- send_sms/main.py
+++ send_sms/main.py
@@ -11,9 +11,11 @@ PHONE_NUMBER = '+16692154466'
 
 def send_sms(request):
     message = json.loads(base64.b64decode(request.json['message']['data']).decode('utf-8'))
-    if 'sender' not in message or not message['sender'] or 'value' not in message:
+    if 'sender' not in message or not message['sender'] or 'value' not in message['sender']:
         message['sender'] = {'value': PHONE_NUMBER}
     print(message)
+    if 'receiver' not in message or not message['receiver'] or 'value' not in message['receiver']:
+        return 'ERROR'
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     client.messages.create(to=message['receiver']['value'], from_=message['sender']['value'],
                            body=message['content'])
