4db80995644540e533bbf91ba21cc3608d681b82
abhi <abhi@kali.care>
2021-08-12 23:53:23 -0700

    Fixed querying sender from data message due to value and id mismatch

--- actions/main.py
+++ actions/main.py
@@ -48,7 +48,7 @@ def process(event, metadata):
     elif channel_name == 'data':
         for data in message['data']:
             context.set('data', {data['name']: data['number'] if 'number' in data else data['value']})
-        context.set('sender', get_resource(message['source'], db))
+        context.set('sender', get_resource({'type': message['source']['type'], 'value': message['source']['id']}, db))
 
     if 'dialogflow' in message :
         context.set('dialogflow', message['dialogflow'])
