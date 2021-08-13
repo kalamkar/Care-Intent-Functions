fe97802939d3aaabe63e981c21fb47cfb9e0f5a6
abhi <abhi@kali.care>
2021-08-12 20:46:06 -0700

    removed debug print statements

--- actions/context.py
+++ actions/context.py
@@ -96,9 +96,7 @@ def merge(destination, source):
     >>> merge(b, a) == { 'first' : { 'all_rows' : { 'pass' : 'dog', 'fail' : 'cat', 'number' : '5' } } }
     True
     """
-    print(destination, source)
     for key, value in source.items():
-        print(key, value)
         if isinstance(value, dict):
             # get node or create one
             if key not in destination or not destination[key]:
