2d3676900b760695f0976d32a7d8e2873efc61b6
abhi <abhi@kali.care>
2021-08-30 21:54:44 -0700

    Updated send sms to support late resolution of sender

--- send_sms/main.py
+++ send_sms/main.py
@@ -1,21 +1,26 @@
 import base64
+
+import common
+import config
 import json
 import logging
 
 from twilio.rest import Client
 
+from google.cloud import firestore
 import google.cloud.logging as logger
 logger.handlers.setup_logging(logger.Client().get_default_handler())
 
-PROJECT_ID = 'careintent'  # os.environ.get('GCP_PROJECT')  # Only for py3.7
 TWILIO_ACCOUNT_SID = 'ACd3f03d1554da132e550d541480419d42'
 TWILIO_AUTH_TOKEN = 'c05ceb45e0fc570aa45643e3ddbb0308'
-PHONE_NUMBER = '+16692154466'
 
 
 def send_sms(request):
+    db = firestore.Client()
     message = json.loads(base64.b64decode(request.json['message']['data']).decode('utf-8'))
-    sender = message['sender']['value'] if 'sender' in message else PHONE_NUMBER
+    sender = common.get_phone_id(message['sender']['value'], db, resource_types=['group'])\
+        if 'sender' in message else config.PHONE_NUMBER
+    sender = sender['value'] if sender else config.PHONE_NUMBER
     receiver = message['receiver']['value'] if 'receiver' in message else None
     logging.info('{} {} {}'.format(message, sender, receiver))
     if not receiver or 'content' not in message or not message['content'] or type(message['content']) != str:
