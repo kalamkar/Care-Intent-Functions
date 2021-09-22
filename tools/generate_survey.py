dfd837c0d3707e4705a710f36a61ea1c64e13161
abhi <abhi@kali.care>
2021-09-21 21:48:19 -0700

    Added tickets, tags and data recording capabilities

--- tools/generate_survey.py
+++ tools/generate_survey.py
@@ -3,11 +3,9 @@ import json
 import sys
 
 
-def main(filename):
+def csv2actions(csv_dict_reader):
     actions = []
-    reader = csv.DictReader(open(filename))
-    intro = True
-    for row in reader:
+    for row in csv_dict_reader:
         condition = ''
         questions = row['Previous Question'].split('\n')
         answers = row['Previous Answer'].split('\n')
@@ -25,20 +23,45 @@ def main(filename):
             else:
                 pairs.append('(person.session.last_question == "{}")'.format(question))
         if not condition and pairs:
-            condition = '{{(message.status == "received") and (%s)}}' % ' or '.join(pairs)
-        actions.append({
-            'id': row['Question'] + '.message',
-            'type': 'Message',
-            'priority': 10,
-            'condition': condition,
-            'min_action_priority': 9,
-            'params': {
-                'content': row['Message'],
-                'receiver': '$person.id'
-            }
-        })
-        if intro:
-          intro = False
+            condition = '{{from_member and (%s)}}' % ' or '.join(pairs)
+        special = row['Special'].split('\n')
+        if 'nomessage' not in special:
+            actions.append({
+                'id': row['Question'] + '.message',
+                'type': 'Message',
+                'priority': 10,
+                'condition': condition,
+                'min_action_priority': 9,
+                'params': {
+                    'content': row['Message'],
+                    'receiver': '$person.id'
+                }
+            })
+        if 'ticket' in special:
+            actions.append({
+                'id': row['Question'],
+                'type': 'OpenTicket',
+                'priority': 10,
+                'condition': condition,
+                'params': {
+                    'content': row['Message'],
+                    'category': row['Tag'],
+                    'person_id': '$person.id'
+                }
+            })
+        if 'profile' in special:
+            actions.append({
+                'id': row['Question'] + '.profile',
+                'type': 'UpdateResource',
+                'priority': 10,
+                'condition': condition,
+                'params': {
+                    'content': row['Tag'],
+                    'list_name': 'tags',
+                    'identifier': '$person.id'
+                }
+            })
+        if 'start' in special:
           actions.append({
               'id': row['Question'] + '.update',
               'type': 'UpdateResource',
@@ -51,20 +74,31 @@ def main(filename):
                   'identifier': '$person.id'
               }
           })
-        elif row['Skip Session Update'] != '1' or row['Exit'] == '1':
+        elif 'noupdate' not in special or 'end' in special:
             actions.append({
                 'id': row['Question'] + '.update',
                 'type': 'UpdateResource',
                 'priority': 9,
                 'condition': condition,
                 'params': {
-                    'content': '{"session.last_question": "%s"}' % (row['Question'] if row['Exit'] != '1' else ''),
+                    'content': '{"session.last_question": "%s"}' % (row['Question'] if 'end' not in special else ''),
                     'identifier': '$person.id'
                 }
             })
 
-    json.dump({'actions': actions}, open(filename + '.json', 'w'), indent=2)
+    actions.append({
+        'id': 'survey.answer.record',
+        'type': 'UpdateData',
+        'priority': 9,
+        'condition': '{{from_member and person.session.last_question != ""}}',
+        'params': {
+            'content': '{"{{person.session.last_question}}": "{{message.content}}"}',
+            'tags': 'survey',
+            'source_id': '$person.id'
+        }
+    })
+    return actions
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    json.dump({'actions': csv2actions(csv.DictReader(open(sys.argv[1])))}, open(sys.argv[1] + '.json', 'w'), indent=2)
