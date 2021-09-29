494fd1c6dd7194e647c15ca8d8f10984717e038a
abhi <abhi@kali.care>
2021-09-29 13:48:05 -0700

    Added a check for empty content while updating resource

--- actions/generic.py
+++ actions/generic.py
@@ -89,6 +89,9 @@ class UpdateResource(Action):
         logging.info('Updating {id} with {data}'.format(id=doc_ref.path, data=content))
         content = json.loads(content.replace('\'', '"').replace(',]', ']'), strict=False, object_hook=lambda d:
             (d | {'start': dateutil.parser.parse(d['start']).astimezone(pytz.utc)}) if 'start' in d else d)
+        if not content:
+            logging.warning('Empty content ' + str(content))
+            return
         doc_ref.update({list_name: firestore.ArrayUnion(content)} if list_name else content)
 
 
