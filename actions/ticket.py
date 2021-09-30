e5aa771e5692d33cc1f38fe525f78212fc04e71b
abhi <abhi@kali.care>
2021-09-30 12:25:51 -0700

    Updated list tickets to prioritize the tickets and get tickets for coach and group

--- actions/ticket.py
+++ actions/ticket.py
@@ -1,41 +1,77 @@
-import logging
+import common
 import config
 import datetime
 import json
+import logging
+import numpy as np
 
 from google.cloud import bigquery
+from google.cloud import firestore
 from google.cloud import pubsub_v1
 
 from generic import Action
 
 
 class List(Action):
-    def process(self, person_id=None):
-        if not person_id or 'value' not in person_id:
-            logging.error('Missing person_id for list tickets')
+    def process(self, person_id=None, parent_id=None, maxlen=5):
+        if (not person_id or 'value' not in person_id) and (not parent_id or 'value' not in parent_id):
+            logging.error('Missing person_id or parent_id for list tickets')
             return
 
-        tickets = {}
+        db = firestore.Client()
+        sources = []
+        if person_id:
+            sources.append(person_id['value'])
+        elif parent_id:
+            sources.extend([pid['value'] for pid in common.get_children_ids(parent_id, 'member', db)])
+
+        tickets = self.get_tickets_from_top_persons(self.get_open_tickets(sources), maxlen) if parent_id \
+            else self.get_top_tickets_from_person(self.get_open_tickets(sources), sources[0], maxlen)
+        self.context_update = {'tickets': tickets}
+
+    @staticmethod
+    def get_top_tickets_from_person(tickets, person, maxlen):
+        return sorted(list(tickets[person].values()), key=lambda t: t['priority'], reverse=True)[:maxlen]
+
+    @staticmethod
+    def get_tickets_from_top_persons(open_tickets, maxlen):
+        person_scores = {person: np.median([t['priority'] for t in tickets.values()]) if len(tickets) else 0
+                         for person, tickets in open_tickets.items()}
+        persons = [person for person, score in sorted(person_scores.items(), key=lambda kv: kv[1], reverse=True)][:maxlen]
+        top_tickets = []
+        for person in persons:
+            tickets = sorted(open_tickets[person].values(), key=lambda t: t['priority'], reverse=True)
+            if tickets:
+                top_tickets.append(tickets[0])
+        return top_tickets
+
+    @staticmethod
+    def get_open_tickets(sources):
         bq = bigquery.Client()
-        q = 'SELECT time, id, status, category, title FROM (' \
-            'SELECT time, ' \
+        q = 'SELECT time, source, id, priority, status, category, title FROM (' \
+            'SELECT time, source, ' \
             '(select number FROM UNNEST(data) WHERE name = "id") as id, '\
             '(select value FROM UNNEST(data) WHERE name = "id") as status, '\
+            '(select number FROM UNNEST(data) WHERE name = "priority") as priority, ' \
             '(select value FROM UNNEST(data) WHERE name = "category") as category, '\
-            '(select value FROM UNNEST(data) WHERE name = "title") as title '\
+            '(select value FROM UNNEST(data) WHERE name = "title") as title ' \
             'FROM {project}.live.tsdata '\
-            'WHERE source.value = "{source}" AND "ticket" IN UNNEST(tags)) '\
-            'ORDER BY time'.format(project=config.PROJECT_ID, source=person_id['value'])
+            'WHERE source.value IN ("{sources}") AND "ticket" IN UNNEST(tags)) '\
+            'ORDER BY time'.format(project=config.PROJECT_ID, sources='","'.join(sources))
+        tickets = {person: {} for person in sources}
         for row in bq.query(q):
+            person = row['source']['value']
+            ticket_id = int(row['id'])
             if row['status'] == 'opened':
-                tickets[int(row['id'])] = {'time': row['time'].isoformat(), 'category': row['category'],
-                                           'title': row['title']}
+                tickets[person][ticket_id] = {'person_id': row['source'], 'id': ticket_id, 'title': row['title'],
+                                              'priority': row['priority'] if row['priority'] else 0,
+                                              'time': row['time'].isoformat(), 'category': row['category']}
             elif row['status'] == 'closed':
-                if int(row['id']) not in tickets:
-                    logging.error('Ticket {} for {} closed before opening'.format(row['id'], person_id))
+                if ticket_id not in tickets[person]:
+                    logging.error('Ticket {} for {} closed before opening'.format(ticket_id, person))
                     continue
-                del tickets[int(row['id'])]
-        self.context_update = {'tickets': tickets}
+                del tickets[person][ticket_id]
+        return tickets
 
 
 class Operation(Action):
