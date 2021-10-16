e3b33cdb73d46a3d29b58f57cbcf48844384d132
abhi <abhi@kali.care>
2021-10-16 09:50:37 -0700

    Refactored short url to be app engine service to be served on custom domain

--- short_url/main.py
+++ short_url/main.py
@@ -1,16 +1,16 @@
 import flask
 from google.cloud import firestore
 
-PROJECT_ID = 'careintent'  # os.environ.get('GCP_PROJECT')  # Only for py3.7
+app = flask.Flask(__name__)
 
 
-def main(request):
+@app.route('/<path>')
+def main(path):
     db = firestore.Client()
-    url = db.collection('urls').document(request.path[1:]).get()
+    url = db.collection('urls').document(path).get()
     response = flask.make_response()
     if not url or not url.get('redirect'):
         response.status_code = 404
         return response
 
     return flask.redirect(url.get('redirect'), 302)
-
