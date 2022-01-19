1bb4deb2c63ba0ea4bb22269ccc8cbdbc9e21fad
abhi <abhi@kali.care>
2022-01-19 12:51:02 -0800

    Updated engage function to listen to pubsub messages instead of http

--- engage/main.py
+++ engage/main.py
@@ -1,8 +1,16 @@
+import base64
+import json
+import logging
 
 import google.cloud.logging as logger
 logger.handlers.setup_logging(logger.Client().get_default_handler())
 
 
-def main(request):
+def main(event, context):
+    logging.info(event)
+    logging.info(context)
+    message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    logging.info(message)
+
     return 'OK'
 
