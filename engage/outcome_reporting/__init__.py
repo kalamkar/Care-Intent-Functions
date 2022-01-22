7dacc5077d8309ee641ac7a41625545e5932d9f9
abhi <abhi@kali.care>
2022-01-22 10:51:56 -0800

    Added missing can_start methods

--- engage/outcome_reporting/__init__.py
+++ engage/outcome_reporting/__init__.py
@@ -2,5 +2,8 @@ from conversation import Conversation as BaseConversation
 
 
 class Conversation(BaseConversation):
+    def can_start(self):
+        return False
+
     def process(self):
         pass
