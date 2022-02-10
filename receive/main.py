ecbb3f2efdd2182e7f3209325e2f3e5110f5f9bc
abhi <abhi@kali.care>
2022-02-10 15:46:30 -0800

    Switched back to dialogflow 1.1.1 since params conversion MessageDict doesn't work in 2.0+

--- receive/main.py
+++ receive/main.py
@@ -6,7 +6,7 @@ import logging
 import pytz
 import twilio.rest
 
-from google.cloud import dialogflow_v2beta1 as dialogflow
+import dialogflow_v2beta1 as dialogflow
 from google.cloud import firestore
 from google.cloud import pubsub_v1
 from google.protobuf.json_format import MessageToDict
@@ -132,9 +132,9 @@ def process_text(sender_id, receiver_id, content, tags, person, db):
         del person['session']['lead']
 
     df_client = dialogflow.SessionsClient()
-    text_input = dialogflow.types.TextInput({'text': content[:255], 'language_code': 'en-US'})
-    df = df_client.detect_intent({'session': df_client.session_path(config.PROJECT_ID, person_id),
-                                  'query_input': dialogflow.types.QueryInput({'text': text_input})})
+    text_input = dialogflow.types.TextInput(text=content[:255], language_code='en-US')
+    df = df_client.detect_intent(session=df_client.session_path(config.PROJECT_ID, person_id),
+                                 query_input=dialogflow.types.QueryInput(text=text_input))
     sentiment_score = df.query_result.sentiment_analysis_result.query_text_sentiment.score
 
     data = {
@@ -151,14 +151,11 @@ def process_text(sender_id, receiver_id, content, tags, person, db):
             'action': df.query_result.action,
             'sentiment_score': sentiment_score,
             'reply': df.query_result.fulfillment_text,
-            'confidence': int(df.query_result.intent_detection_confidence * 100)
+            'confidence': int(df.query_result.intent_detection_confidence * 100),
+            'params': MessageToDict(df.query_result.parameters)
         }
     }
-
-    try:
-        data['nlp']['params'] = MessageToDict(df.query_result.parameters)
-    except:
-        logging.error('Could not convert parameters')
+    # TODO: Find alternative for MessageToDict, it does not work once updated to google-cloud-dialogflow
 
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(config.PROJECT_ID, 'message')
