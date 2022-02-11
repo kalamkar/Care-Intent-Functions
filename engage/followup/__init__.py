20d473448f3a7421e616c92fa3beef22a2df4a1c
abhi <abhi@kali.care>
2022-02-11 14:22:34 -0800

    Added conditional nudge message for followup. Changed followup conv end mechanism

--- engage/followup/__init__.py
+++ engage/followup/__init__.py
@@ -9,8 +9,6 @@ from conversation import Conversation as BaseConversation
 
 from google.cloud import bigquery
 
-ENDING_IDS = ['task_confirm_yes', 'task_confirm_no']
-
 
 class Conversation(BaseConversation):
     def __init__(self, config, context):
@@ -21,8 +19,10 @@ class Conversation(BaseConversation):
         now = datetime.datetime.utcnow()
         timezone = self.context.get('person.timezone')
         now = now.astimezone(pytz.timezone(timezone)) if timezone else now
-        now_is_scheduled = self.is_scheduled_time(now)
-        if now_is_scheduled and self.config['check'] == 'tasks':
+        is_scheduled_now = self.is_scheduled_time(now)
+        if is_scheduled_now and 'ended' in self.config:
+            del self.config['ended']
+        if is_scheduled_now and 'check' in self.config and self.config['check'] == 'tasks':
             tasks = self.context.get('person.tasks')
             if not tasks or type(tasks) != dict:
                 logging.info('No tasks for the person')
@@ -33,10 +33,11 @@ class Conversation(BaseConversation):
                 if not self.has_completed(task['schedule'], task['data'], now):
                     self.missing_tasks.append(task)
             return len(self.missing_tasks) > 0
+        elif is_scheduled_now and 'check' in self.config:
+            return self.context.render(self.config['check']) == 'True'
 
         last_message_id = self.context.get('person.last_message_id')
-        return last_message_id and last_message_id.startswith(self.__module__)\
-               and last_message_id.split('.')[1] not in ENDING_IDS
+        return last_message_id and last_message_id.startswith(self.__module__) and 'ended' not in self.config
 
     def process(self):
         last_message_id = self.context.get('person.last_message_id')
@@ -49,12 +50,17 @@ class Conversation(BaseConversation):
                 self.publish_data(source_id=self.context.get('person.id'), tags='biometrics',
                                   params={'time': '', 'medication': ''})
                 self.message_id = ['task_confirm_yes', last_message_id.split('.')[-1]]
+                self.config['ended'] = True
             elif df.query_result.intent.display_name == 'generic.no':
                 self.message_id = ['task_confirm_no']
+                self.config['ended'] = True
             else:
                 logging.warning('Unexpected result {}'.format(df.query_result))
                 self.skip_message_id_update = True
                 self.message_id = ['confirm_yes']
+        else:
+            self.message_id = [self.config['message_id']]
+            self.config['ended'] = True
 
     def has_completed(self, schedule, data, now):
         cron = croniter.croniter(schedule, now)
