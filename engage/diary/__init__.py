eb70d89eef8f974511efdd761a49443908a8bf1e
abhi <abhi@kali.care>
2022-02-10 19:45:21 -0800

    Using messages module for diary

--- engage/diary/__init__.py
+++ engage/diary/__init__.py
@@ -4,17 +4,6 @@ from conversation import Conversation as BaseConversation
 
 INTENTS = [r'^biomarker', r'^medication\.report']
 
-DATA_CONFIG = {
-    'hip': {'label': 'Hip\'s circumference', 'low': 0, 'high': 0},
-    'waist': {'label': 'Waist\'s circumference', 'low': 0, 'high': 0},
-    'weight': {'label': 'Weight', 'low': 0, 'high': 0},
-    'height': {'label': 'Height', 'low': 0, 'high': 0},
-    'temperature': {'label': 'Temperature', 'low': 0, 'high': 0},
-    'a1c': {'label': 'A1C (blood sugar level)', 'low': 0, 'high': 0},
-    'diastolic': {'label': 'Blood pressure', 'low': 0, 'high': 0},
-    'systolic': {'label': 'Blood pressure', 'low': 0, 'high': 0}
-}
-
 
 class Conversation(BaseConversation):
     def can_process(self):
@@ -24,7 +13,5 @@ class Conversation(BaseConversation):
     def process(self):
         params = self.context.get('message.nlp.params')
         self.publish_data(source_id=self.context.get('person.id'), tags='biometrics', params=params)
-        param_name = list(params.keys())[0]
-        self.reply = 'Your {} has been recorded'.format(
-            DATA_CONFIG[param_name]['label'].lower() if param_name in DATA_CONFIG else 'data')
-        # self.config['last_message_type'] = 'q1'
+        param_name = sorted(list(params.keys()))[0]
+        self.message_id = ['recorded', param_name]
--- engage/messages.py
+++ engage/messages.py
@@ -1,15 +1,23 @@
 
 DATA = {
-    'assessment': 'How are you?',
-    'barrier': 'Why?',
-    'chitchat': 'Good to hear from you.',
-    'diary': 'Got it!',
-    'education': 'Hello',
-    'feedback': 'Hi',
+    'diary.recorded.hip': 'Your hip\'s circumference has been recorded',
+    'diary.recorded.waist': 'Your waist\'s circumference has been recorded',
+    'diary.recorded.weight': 'Your weight has been recorded',
+    'diary.recorded.height': 'Your height has been recorded',
+    'diary.recorded.temperature': 'Your temperature has been recorded',
+    'diary.recorded.a1c': 'Your A1C (blood sugar level) has been recorded',
+    'diary.recorded.diastolic': 'Your blood pressure has been recorded',
 
     'feedback.task_confirm.generic': 'Did you do it?',
     'feedback.task_confirm.medication': 'Did you take the medication?',
     'feedback.task_confirm_yes.generic': 'Nice, next time let us know.',
     'feedback.task_confirm_yes.medication': 'Nice, next time text X whenever you take your medication.',
-    'feedback.confirm_yes': 'Is that a yes?'
+    'feedback.confirm_yes': 'Is that a yes?',
+    
+    'assessment': 'How are you?',
+    'barrier': 'Why?',
+    'chitchat': 'Good to hear from you.',
+    'diary': 'Got it!',
+    'education': 'Hello',
+    'feedback': 'Hi'
 }
