6dd7bfae2ace25dd2c86552edc3131e1ac8a3cf8
abhi <abhi@kali.care>
2022-04-05 15:16:15 -0700

    Added did u take med on 2nd missing day also and not sending ed msg if task completed

--- engage/followup/__init__.py
+++ engage/followup/__init__.py
@@ -1,7 +1,7 @@
-import croniter
-
 import common
 import config
+import croniter
+import dateutil.parser
 import datetime
 import logging
 import pytz
@@ -13,9 +13,6 @@ from google.cloud import bigquery
 
 class Conversation(BaseConversation):
     def can_process(self):
-        now = datetime.datetime.utcnow()
-        timezone = self.context.get('person.timezone')
-        now = now.astimezone(pytz.timezone(timezone)) if timezone else now
         is_scheduled_now = self.is_scheduled_now()
         last_message_id = self.context.get('person.last_message_id')
 
@@ -25,31 +22,40 @@ class Conversation(BaseConversation):
         if is_scheduled_now and 'repeat_question' in self.config:
             return True
         elif is_scheduled_now and 'check' in self.config and self.config['check'] == 'tasks':
-            is_missing_task = False
-            tasks = self.context.get('person.tasks')
-            if not tasks or type(tasks) != list:
-                logging.info('No tasks for the person')
-                return False
-            for task in tasks:
-                if 'data' not in task or 'schedule' not in task:
-                    continue
-                last_completed_time = self.last_completed(self.context.get('person.id.value'), task['data'])
-                last_completed_time = last_completed_time.astimezone(pytz.timezone(timezone))\
-                    if timezone else last_completed_time
-                last_expected_time = croniter.croniter(task['schedule'], now).get_prev(datetime.datetime)
-                tolerance = datetime.timedelta(seconds=(3600 if 'tolerance' not in task else
-                                                        common.get_duration_secs(task['tolerance'])))
-                if last_completed_time < (last_expected_time - tolerance):
-                    self.context.set('missing_task', task | {'last_completed_time': last_completed_time})
-                    is_missing_task = True
-            return is_missing_task
+            return self.is_missing_task()
         elif is_scheduled_now and 'check' in self.config:
-            return self.context.render(self.config['check']) == 'True'
+            return self.is_missing_task()
 
         return last_message_id and last_message_id.startswith(self.__module__) and 'ended' not in self.config and \
                not self.is_scheduled_run()
 
+    def is_missing_task(self):
+        now = datetime.datetime.utcnow()
+        timezone = self.context.get('person.timezone')
+        now = now.astimezone(pytz.timezone(timezone)) if timezone else now
+
+        is_missing_task = False
+        tasks = self.context.get('person.tasks')
+        if not tasks or type(tasks) != list:
+            logging.info('No tasks for the person')
+            return False
+        for task in tasks:
+            if 'data' not in task or 'schedule' not in task:
+                continue
+            last_completed_time = self.last_completed(self.context.get('person.id.value'), task['data'])
+            last_completed_time = last_completed_time.astimezone(pytz.timezone(timezone)) \
+                if timezone else last_completed_time
+            self.config['last_completed_hours'] = int((now - last_completed_time).total_seconds() / 3600)
+            last_expected_time = croniter.croniter(task['schedule'], now).get_prev(datetime.datetime)
+            tolerance = datetime.timedelta(seconds=(3600 if 'tolerance' not in task else
+                                                    common.get_duration_secs(task['tolerance'])))
+            if last_completed_time < (last_expected_time - tolerance):
+                self.context.set('missing_task', task)
+                is_missing_task = True
+        return is_missing_task
+
     def process(self):
+        last_completed_hours = self.config['last_completed_hours'] if 'last_completed_hours' in self.config else 1000
         last_message_id = self.context.get('person.last_message_id')
         content_type = self.context.get('message.content_type')
         missing_task = self.context.get('missing_task')
@@ -61,14 +67,10 @@ class Conversation(BaseConversation):
             del self.config['repeat_question']
         elif self.config['check'] == 'tasks' and missing_task:
             task_type = missing_task['data'] if 'data' in missing_task else 'generic'
-            if 'prev_message' in self.config and self.config['prev_message'] == 'task_confirm':
-                self.transfer_type = 'barriers'
-                del self.config['prev_message']
-            else:
-                self.message_id = ['task_confirm', task_type]
-                self.config['prev_message'] = 'task_confirm'
-                if 'repeat' in self.config and self.config['repeat']:
-                    self.config['repeat_question'] = True
+            self.message_id = ['task_confirm', task_type]
+            self.config['prev_message'] = 'task_confirm'
+            if 'repeat' in self.config and self.config['repeat']:
+                self.config['repeat_question'] = True
         elif not self.is_scheduled_now() and last_message_id and content_type != 'application/json' and\
                 last_message_id.startswith(self.__module__ + '.task_confirm'):
             if 'repeat_question' in self.config:
@@ -79,6 +81,8 @@ class Conversation(BaseConversation):
                                   params={'time': '', 'medication': ''})
                 self.message_id = ['task_confirm_yes', last_message_id.split('.')[-1]]
                 self.config['ended'] = True
+            elif df.query_result.intent.display_name == 'generic.no' and last_completed_hours > 40:
+                self.transfer_type = 'barriers'
             elif df.query_result.intent.display_name == 'generic.no':
                 self.message_id = ['task_confirm_no']
                 self.config['ended'] = True
